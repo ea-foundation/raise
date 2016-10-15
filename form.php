adb948561f79c7fb2058bc981314db416647663c
Naoki Peter <knockey@gmail.com>
2016-10-15 03:16:12 +0200

    Added new translation

--- form.php
+++ form.php
@@ -253,7 +253,7 @@ function donationForm($atts, $content = null)
                     <div class="col-sm-offset-3 col-sm-9">
                         <div class="checkbox">
                             <label>
-                                <input type="checkbox" name="tax_receipt" id="tax-receipt" value="1"> <?php _e('I need a tax receipt for Germany or Switzerland.', 'eas-donation-processor') ?>
+                                <input type="checkbox" name="tax_receipt" id="tax-receipt" value="1"> <?php _e('I need a tax receipt for Germany or Switzerland', 'eas-donation-processor') ?>
                             </label>
                         </div>
                     </div>
