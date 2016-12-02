e778960a922bc409b45632b6a92b5cb32daae449
Naoki Peter <knockey@gmail.com>
2016-12-02 12:46:46 +0100

    Tweaked CSS

--- eas-donation-processor.php
+++ eas-donation-processor.php
@@ -3,7 +3,7 @@
  * Plugin Name: EAS Donation Processor
  * Plugin URI: https://github.com/GBS-Schweiz/eas-donation-processor
  * Description: Process donations
- * Version: 0.1.5
+ * Version: 0.1.6
  * Author: Naoki Peter
  * Author URI: http://www.0x1.ch
  * License: proprietary
