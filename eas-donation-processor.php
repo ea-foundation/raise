c1fc32185cc92cbf0772942b52c97fa282e9bf38
Naoki Peter <knockey@gmail.com>
2017-01-26 15:24:14 +0100

    Updated version number

--- eas-donation-processor.php
+++ eas-donation-processor.php
@@ -3,7 +3,7 @@
  * Plugin Name: EAS Donation Processor
  * Plugin URI: https://github.com/GBS-Schweiz/eas-donation-processor
  * Description: Process donations
- * Version: 0.2.6
+ * Version: 0.2.7
  * Author: Naoki Peter
  * Author URI: http://0x1.ch
  * License: proprietary
