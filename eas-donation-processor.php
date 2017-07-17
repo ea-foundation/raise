e76baf7b56bc3977cd307f09236e51c16a2e4b50
Naoki Peter <knockey@gmail.com>
2017-07-17 11:28:20 +0200

    Fixed JavaScript dependencies

--- eas-donation-processor.php
+++ eas-donation-processor.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/ea-foundation/eas-donation-processor
  * GitHub Plugin URI: ea-foundation/eas-donation-processor
  * Description: Process donations
- * Version: 0.8.1
+ * Version: 0.8.2
  * Author: Naoki Peter
  * Author URI: http://0x1.ch
  * License: proprietary
@@ -16,7 +16,7 @@ defined('ABSPATH') or die('No script kiddies please!');
 define('EAS_PRIORITY', 12838790321);
 
 // Asset version
-define('EAS_ASSET_VERSION', '0.24');
+define('EAS_ASSET_VERSION', '0.25');
 
 // Load other files
 require_once 'vendor/autoload.php';
@@ -141,7 +141,7 @@ function register_donation_scripts()
     wp_register_script('donation-plugin-stripe', '//checkout.stripe.com/checkout.js');
     wp_register_script('donation-plugin-paypal', '//www.paypalobjects.com/api/checkout.v4.js');
     wp_register_script('donation-combobox', plugins_url('eas-donation-processor/js/bootstrap-combobox.js'), array(), EAS_ASSET_VERSION);
-    wp_register_script('donation-plugin-form', plugins_url('eas-donation-processor/js/form.js'), array('jquery', 'donation-plugin-stripe'), EAS_ASSET_VERSION);
+    wp_register_script('donation-plugin-form', plugins_url('eas-donation-processor/js/form.js'), array('jquery', 'donation-plugin-stripe', 'donation-plugin-paypal'), EAS_ASSET_VERSION);
 }
 
 // Register fundraiser post type
