afd5f2bea5854c3ceaa0be261d469773599f0da5
Kim Korte <psaniko@gmail.com>
2018-05-04 09:42:11 +0200

    Bump version to 1.3.4

--- raise.php
+++ raise.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/ea-foundation/raise
  * GitHub Plugin URI: ea-foundation/raise
  * Description: The Free Donation Plugin for WordPress
- * Version: 1.3.3
+ * Version: 1.3.4
  * Author: Naoki Peter
  * License: GPLv3 or later
  */
