394862455f996309ce1a73cc9be4201ec53c42ce
naokipeter <naoki.l.peter@gmail.com>
2018-05-16 00:07:36 +0200

    Fix mailing list label and share data label display

--- raise.php
+++ raise.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/ea-foundation/raise
  * GitHub Plugin URI: ea-foundation/raise
  * Description: The Free Donation Plugin for WordPress
- * Version: 1.3.2
+ * Version: 1.3.5
  * Author: Naoki Peter
  * License: GPLv3 or later
  */
@@ -15,7 +15,7 @@ defined('ABSPATH') or exit;
 define('RAISE_PRIORITY', 12838790321);
 
 // Asset version
-define('RAISE_ASSET_VERSION', '0.44');
+define('RAISE_ASSET_VERSION', '0.45');
 
 // Load other files
 require_once "vendor/autoload.php";
